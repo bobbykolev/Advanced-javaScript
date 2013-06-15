a79146987d1e1f56f66801c60239e907c55cc745
Bobby Kolev <bobbykolev@abv.bg>
2013-06-15 23:12:28 +0300

    remove extend

--- 02.jQuery/01.Slider/script.js
+++ 02.jQuery/01.Slider/script.js
@@ -73,15 +73,3 @@ if (!Object.create) {
     return new f();
   }
 }
-
-if (!Object.prototype.extend) {
-    Object.prototype.extend = function(properties) {
-        function f() {};
-        f.prototype = Object.create(this);
-        for(var prop in properties) {
-            f.prototype[prop] = properties[prop];
-        }
-        f.prototype._super = this;
-        return new f();
-    }
-}
