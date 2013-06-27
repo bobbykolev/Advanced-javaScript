794d7321b620a52f4057329811bab60a8112449a
Bobby Kolev <bobbykolev@abv.bg>
2013-06-27 21:43:58 +0300

    treeview plugin

---
+++ 03.jQueryPlugin/js/script.js
@@ -0,0 +1,21 @@
+(function($, undefined) {
+
+    $.fn.treeview = function(options) {
+        if(!options) 
+        {
+            var options = {};
+        }
+        var defaults = { 
+            collapsed: (options.collapsed != undefined)? options.collapsed : true, 
+            speed: options.speed || 500
+        };
+            this.find('li>ul').show();
+            if(defaults.collapsed == true) {
+            this.find('li>ul').hide();
+        }
+        return this.on('click', 'span', function(ev) {
+            ev.stopPropagation();
+            $(this).parent().find('>ul').toggle(defaults.speed);
+        });
+    }   
+})(jQuery);
