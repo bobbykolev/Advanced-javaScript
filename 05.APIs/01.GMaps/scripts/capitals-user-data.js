b35814a153e3584bc1ba84c843e22bc4118873ba
Bobby Kolev <bobbykolev@abv.bg>
2013-06-30 00:16:51 +0300

    google maps - european capitals slider project

---
+++ 05.APIs/01.GMaps/scripts/capitals-user-data.js
@@ -0,0 +1,71 @@
+var listOfCapitalsData = [
+{
+    "lat": 42.6978388,
+    "lon": 23.3216697,
+    "title": "Sofia, Bulgaria",
+    "info": "Sofia",
+    "img": "images/sofia.jpg"
+},
+{
+    "lat": 40.33084263909575,
+    "lon": -3.6749267578125,
+    "title": "Madrid, Spain",
+    "info": "Madrid",
+    "img": "images/madrid.jpg"
+},
+{
+    "lat": 48.856614,
+    "lon": 2.3522219,
+    "title": "Paris, France",
+    "info": "Paris",
+    "img": "images/paris.jpg"
+},
+{
+    "lat": 52.523741,
+    "lon": 13.412246,
+    "title": "Berlin, Germany",
+    "info": "Berlin",
+    "img": "images/berlin.jpg"
+},
+{
+    "lat": 41.89001042401827,
+    "lon": 12.490425109863281,
+    "title": "Rome, Italy",
+    "info": "Rome",
+    "img": "images/rome.jpg"
+},
+{
+    "lat": 38.7252993,
+    "lon": -9.1500364,
+    "title": "Lisbon, Portugal",
+    "info": "Lisbon",
+    "img": "images/lisbon.jpg"
+},
+{
+    "lat": 46.9479222,
+    "lon": 7.4446085,
+    "title": "Bern, Switzerland",
+    "info": "Bern",
+    "img": "images/bern.jpg"
+},
+{
+    "lat": 51.5112139,
+    "lon": -0.1198244,
+    "title": "London, England",
+    "info": "London",
+    "img": "images/london.jpg"
+},
+{
+    "lat": 45.8130293,
+    "lon": 15.9778949,
+    "title": "Zagreb, Croatia",
+    "info": "Zagreb",
+    "img": "images/zagreb.jpg"
+},
+{
+    "lat": 48.210032,
+    "lon": 16.369972,
+    "title": "Vienna, Austria",
+    "info": "Vienna",
+    "img": "images/viena.jpg"
+}];
---
+++ 05.APIs/01.GMaps/scripts/script.js
@@ -0,0 +1,100 @@
+var map;
+$(document).ready(function () {
+
+    $('#maps-container').append('<div id="left-arrow"></div><div id="right-arrow"></div><div id="map-border" class="0"><div id="map"></div></div><nav id="maps-list-container"><ul id="maps-list"></ul></nav>');
+
+    var MapClass = {
+        init: function (data) {
+            this.data = data
+        },
+        renderThumbs: function (selector) {
+            this.selector = selector;
+            for (var j = 0, len = this.data.length; j < len; j++) {
+                $(this.selector).append('<li><h3>' + this.data[j].title + '</h3><img src="' + this.data[j].img + '" id="' + j + '"/></li>');
+            }
+        },
+        renderMap: function (index) {
+
+            function initialize() {
+                this.index = index;
+                var mapOptions = {
+                    zoom: 12,
+                    center: new google.maps.LatLng(listOfCapitalsData[this.index].lat, listOfCapitalsData[this.index].lon),
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+                $('#map').empty();
+                map = new google.maps.Map(document.getElementById('map'),
+                   mapOptions);
+
+                var marker = new google.maps.Marker({
+                    position: map.getCenter(),
+                    map: map,
+                    title: listOfCapitalsData[this.index].title
+                });
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: listOfCapitalsData[this.index].info
+                });
+
+                google.maps.event.addListener(marker, 'click', function () {
+                    infowindow.open(map, marker);
+                    map.panTo(marker.getPosition());
+                });
+
+            }
+            google.maps.event.addDomListener(window, 'load', initialize());
+        }
+    };
+
+    var theMap = Object.create(MapClass);
+
+    theMap.init(listOfCapitalsData);
+    theMap.renderThumbs('#maps-list');
+    theMap.renderMap(0);
+
+    $('li>img').on('click', function () {
+        var i = this.id;
+        theMap.renderMap(i);
+        $('#map-border').attr('class', i);
+    });
+    function next() {
+        var i = $("#map-border").attr("class");
+        if (i == listOfCapitalsData.length - 1) {
+            i = -1;
+        };
+        theMap.renderMap(parseInt(i) + 1);
+        $("#map-border").attr("class", parseInt(i) + 1)
+    }
+    function prev() {
+        var i = $("#map-border").attr("class");
+        if (i == -1) {
+            i = listOfCapitalsData.length;
+        };
+        theMap.renderMap(parseInt(i) - 1);
+        $("#map-border").attr("class", parseInt(i) - 1)
+    }
+
+    $("#left-arrow").on('click', prev);
+    $("#right-arrow").on('click', next);
+
+    $("#left-arrow").on('mousedown', function () {
+        $("#left-arrow").css({ 'background': 'url(images/left_a.png)' });
+    });
+    $("#left-arrow").on('mouseup', function () {
+        $("#left-arrow").css({ 'background': 'url(images/left.png)' });
+    });
+    $("#right-arrow").on('mousedown', function () {
+        $("#right-arrow").css({ 'background': 'url(images/right_a.png)' });
+    });
+    $("#right-arrow").on('mouseup', function () {
+        $("#right-arrow").css({ 'background': 'url(images/right.png)' });
+    });
+});
+
+if (!Object.create) {
+    Object.create = function (obj) {
+        function f() { };
+        f.prototype = obj;
+        return new f();
+    }
+}
