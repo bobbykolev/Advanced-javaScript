1b4ac06de4261dba7786cd5a998f757e7d75015f
Bobby Kolev <bobbykolev@abv.bg>
2013-06-30 01:12:36 +0300

    style and info changes

--- 05.APIs/01.GMaps/scripts/capitals-user-data.js
+++ 05.APIs/01.GMaps/scripts/capitals-user-data.js
@@ -3,35 +3,35 @@ var listOfCapitalsData = [
     "lat": 42.6978388,
     "lon": 23.3216697,
     "title": "Sofia",
-    "info": 'Sofia',
+    "info": 'Sofia (Bulgarian: София, pronounced [ˈsɔfijɐ]) is the capital and largest city of Bulgaria. Sofia is located at the foot of Mount Vitosha in the western part of the country...',
     "img": "images/sofia.jpg"
 },
 {
     "lat": 40.33084263909575,
     "lon": -3.6749267578125,
     "title": "Madrid",
-    "info": "Madrid",
+    "info": "Madrid (English /məˈdrɪd/, Spanish: [maˈðɾið]) is the capital of Spain and its largest city.It is the third-largest city in the European Union. The city is located on the Manzanares river...",
     "img": "images/madrid.jpg"
 },
 {
     "lat": 48.856614,
     "lon": 2.3522219,
     "title": "Paris",
-    "info": "Paris",
+    "info": "Paris (English /ˈpærɪs/, i/ˈpɛrɪs/; French: [paʁi]) is the capital and most populous city of France. It is situated on the river Seine, in the northern central part of the country...",
     "img": "images/paris.jpg"
 },
 {
     "lat": 52.523741,
     "lon": 13.412246,
     "title": "Berlin",
-    "info": "Berlin",
+    "info": "Berlin (/bərˈlɪn/; German pronunciation: [bɛɐ̯ˈliːn]) is the capital city of Germany. Located in northeastern Germany on the River Spree...",
     "img": "images/berlin.jpg"
 },
 {
     "lat": 41.89001042401827,
     "lon": 12.490425109863281,
     "title": "Rome",
-    "info": "Rome",
+    "info": "Rome (/ˈroʊm/; Italian: Roma pronounced [ˈroːma]; Latin: Rōma) is the capital of Italy. Rome's history spans more than two and a half thousand years, since its legendary founding in 753 BC. It is one of the oldest cities in Europe...",
     "img": "images/rome.jpg"
 },
 {
@@ -66,6 +66,6 @@ var listOfCapitalsData = [
     "lat": 48.210032,
     "lon": 16.369972,
     "title": "Vienna",
-    "info": "Vienna",
+    "info": "Vienna (/viːˈɛnə/; German: Wien [viːn], Austro-Bavarian: Wean) is the capital and largest city of Austria. Apart from being regarded as the City of Music[8] because of its musical legacy, Vienna is also said to be \"The City of Dreams\" because it was home to the world's first psycho-analyst - Sigmund Freud.",
     "img": "images/viena.jpg"
 }];
