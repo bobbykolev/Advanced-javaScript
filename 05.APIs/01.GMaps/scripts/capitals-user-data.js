0aac8f94ea94959904c005009a44a26d66a4d6c5
Bobby Kolev <bobbykolev@abv.bg>
2013-06-30 02:54:18 +0300

    some info added

--- 05.APIs/01.GMaps/scripts/capitals-user-data.js
+++ 05.APIs/01.GMaps/scripts/capitals-user-data.js
@@ -3,69 +3,69 @@ var listOfCapitalsData = [
     "lat": 42.6978388,
     "lon": 23.3216697,
     "title": "Sofia",
-    "info": 'Sofia (Bulgarian: София, pronounced [ˈsɔfijɐ]) is the capital and largest city of Bulgaria. Sofia is located at the foot of Mount Vitosha in the western part of the country...',
+    "info": "Sofia (Bulgarian: София, pronounced [ˈsɔfijɐ]) is the capital and largest city of Bulgaria. Sofia is located at the foot of Mount Vitosha in the western part of the country... <a href='http://en.wikipedia.org/wiki/Sofia' target='_blank'>more</a>",
     "img": "images/sofia.jpg"
 },
 {
     "lat": 40.41611,
     "lon": -3.703187,
     "title": "Madrid",
-    "info": "Madrid (English /məˈdrɪd/, Spanish: [maˈðɾið]) is the capital of Spain and its largest city.It is the third-largest city in the European Union. The city is located on the Manzanares river...",
+    "info": "Madrid (English /məˈdrɪd/, Spanish: [maˈðɾið]) is the capital of Spain and its largest city.It is the third-largest city in the European Union. The city is located on the Manzanares river... <a href='http://en.wikipedia.org/wiki/Madrid' target='_blank'>more</a>",
     "img": "images/madrid.jpg"
 },
 {
     "lat": 48.856614,
     "lon": 2.3522219,
     "title": "Paris",
-    "info": "Paris (English /ˈpærɪs/, i/ˈpɛrɪs/; French: [paʁi]) is the capital and most populous city of France. It is situated on the river Seine, in the northern central part of the country...",
+    "info": "Paris (English /ˈpærɪs/, i/ˈpɛrɪs/; French: [paʁi]) is the capital and most populous city of France. It is situated on the river Seine, in the northern central part of the country... <a href='http://en.wikipedia.org/wiki/Paris' target='_blank'>more</a>",
     "img": "images/paris.jpg"
 },
 {
     "lat": 52.523741,
     "lon": 13.412246,
     "title": "Berlin",
-    "info": "Berlin (/bərˈlɪn/; German pronunciation: [bɛɐ̯ˈliːn]) is the capital city of Germany. Located in northeastern Germany on the River Spree...",
+    "info": "Berlin (/bərˈlɪn/; German pronunciation: [bɛɐ̯ˈliːn]) is the capital city of Germany. Located in northeastern Germany on the River Spree... <a href='http://en.wikipedia.org/wiki/Berlin' target='_blank'>more</a>",
     "img": "images/berlin.jpg"
 },
 {
     "lat": 41.89001042401827,
     "lon": 12.490425109863281,
     "title": "Rome",
-    "info": "Rome (/ˈroʊm/; Italian: Roma pronounced [ˈroːma]; Latin: Rōma) is the capital of Italy. Rome's history spans more than two and a half thousand years, since its legendary founding in 753 BC. It is one of the oldest cities in Europe...",
+    "info": "Rome (/ˈroʊm/; Italian: Roma pronounced [ˈroːma]; Latin: Rōma) is the capital of Italy. Rome's history spans more than two and a half thousand years, since its legendary founding in 753 BC. It is one of the oldest cities in Europe... <a href='http://en.wikipedia.org/wiki/Rome' target='_blank'>more</a>",
     "img": "images/rome.jpg"
 },
 {
     "lat": 38.7252993,
     "lon": -9.1500364,
     "title": "Lisbon",
-    "info": "Lisbon",
+    "info": "Lisbon (/ˈlɪzbən/, liz-bən; Portuguese: Lisboa, IPA: [liʒˈboɐ] or [liʒˈβoɐ]) is the capital city and largest city of Portugal. Lisbon is the westernmost large city located in Europe, as well as its westernmost capital city and the only one along the Atlantic coast... <a href='http://en.wikipedia.org/wiki/Lisbon' target='_blank'>more</a>",
     "img": "images/lisbon.jpg"
 },
 {
     "lat": 46.9479222,
     "lon": 7.4446085,
     "title": "Bern",
-    "info": "Bern",
+    "info": "The city of Bern or Berne (German: Bern, pronounced [bɛrn])  is the capital of Switzerland. In 1983 the historic old town in the centre of Bern became a UNESCO World Heritage Site and Bern is ranked among the world’s top ten cities for the best quality of life... <a href='http://en.wikipedia.org/wiki/Bern' target='_blank'>more</a>",
     "img": "images/bern.jpg"
 },
 {
     "lat": 51.5112139,
     "lon": -0.1198244,
     "title": "London",
-    "info": "London",
+    "info": "London /ˈlʌndən/ is the capital of England and the United Kingdom.Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium... <a href='http://en.wikipedia.org/wiki/London' target='_blank'>more</a>",
     "img": "images/london.jpg"
 },
 {
     "lat": 45.8130293,
     "lon": 15.9778949,
     "title": "Zagreb",
-    "info": "Zagreb",
+    "info": "Zagreb (Croatian pronunciation: [zǎːɡreb]) is the capital and the largest city of the Republic of Croatia. It is located in the northwest of the country, along the Sava river, at the southern slopes of the Medvednica mountain... <a href='http://en.wikipedia.org/wiki/Zagreb' target='_blank'>more</a>",
     "img": "images/zagreb.jpg"
 },
 {
     "lat": 48.210032,
     "lon": 16.369972,
     "title": "Vienna",
-    "info": "Vienna (/viːˈɛnə/; German: Wien [viːn], Austro-Bavarian: Wean) is the capital and largest city of Austria. Apart from being regarded as the City of Music[8] because of its musical legacy, Vienna is also said to be \"The City of Dreams\" because it was home to the world's first psycho-analyst - Sigmund Freud.",
+    "info": "Vienna (/viːˈɛnə/; German: Wien [viːn], Austro-Bavarian: Wean) is the capital and largest city of Austria. Apart from being regarded as the City of Music[8] because of its musical legacy, Vienna is also said to be \"The City of Dreams\" because it was home to the world's first psycho-analyst - Sigmund Freud... <a href='http://en.wikipedia.org/wiki/Viena' target='_blank'>more</a>",
     "img": "images/viena.jpg"
 }];
